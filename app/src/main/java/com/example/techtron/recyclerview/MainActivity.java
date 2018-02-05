903d0e0a687e39f8dfdc914dc88098eb1577307c
Phillip <phillipables1@gmail.com>
2018-02-05 14:24:57 -0500

    ArrayList for school subjects then add my Schoolobjects to the array

--- app/src/main/java/com/example/techtron/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/techtron/recyclerview/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.techtron.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
         //Array of school subject object that it will populate the recycler view with
         ArrayList<SchoolSubject> schoolSubjects = new ArrayList<>();
-        
+
+        SchoolSubject  forensic = new SchoolSubject("Forensic", false, R.drawable.forensic);
+        schoolSubjects.add(forensic);
+
+        SchoolSubject  computerScience = new SchoolSubject("Computer Science", false, R.drawable.computer_science);
+        schoolSubjects.add(computerScience);
+
+        SchoolSubject  network = new SchoolSubject("Network", false, R.drawable.network);
+        schoolSubjects.add(computerScience);
+
+        SchoolSubject  physics = new SchoolSubject("Physics", false, R.drawable.physics);
+        schoolSubjects.add(physics);
+
+
     }
 }
